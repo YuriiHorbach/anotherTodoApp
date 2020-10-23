2dce183f38bbd1726b74d398a4542f31e725b136
YuriiHorbach <gyrii84@gmail.com>
2020-10-23 22:44:46 +0300

    add bootstrap and viewed table on view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
 	def home
-		
+		@tasks = Task.all
 	end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,12 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("jquery")
+// require("@rails/ujs").start()
+// require("turbolinks").start()
+// require("@rails/activestorage").start()
+// require("channels")
+// require("jquery")
+// require("bootstrap")
 
 
 
@@ -17,3 +17,11 @@ require("jquery")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+// import "bootstrap"
+import "bootstrap";
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+
